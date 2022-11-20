8b0ca0c2d6f2eb3c9d2147e6a069d876786242b1
Hunter Powell <hunterp1510@gmail.com>
2022-11-20 00:50:54 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// What is HTTP?
+
+    //Hypertext transfer protocol are the steps followed by the network to connect you to a site.
+
+// What is a URL?
+
+    //A web address to a website
+
+// What is DNS?
+
+    //Domain Name System: it use the URL to find the matching IP address
+
+// What is a query string?
+
+    //Terms you are searching for on a website
+
+// What are two HTTP verbs and how are they different?
+
+    //Get:when you are requesting information
+    //Post:when you give information and it has a side affect
+
+// What is an HTTP request?
+ 
+    // request for data from a server about a website
+
+// What is an HTTP response?
+
+    //the data needed for a computer to build the website for the user
+
+// What is an HTTP header? Give a couple examples of request and response headers you have seen.
+
+    //http headers is information that is being given or asked for
+    //Request: :method:,:authority:,:path:   Response: age, date, server
+
+// What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+
+    // A Get request is made to the IP address that is connected to the host name "somesite.com" and the information at the path name "/some/page" is the 
+    //request
